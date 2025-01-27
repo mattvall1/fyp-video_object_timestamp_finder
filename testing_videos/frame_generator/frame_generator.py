379f1d20f581a9ee7c23bd77b7e8252629fcb4e7
Matthew Vallance <matthew.vallance@outlook.com>
2025-01-27 16:01:47 +0000

    Multithreading SVG conversions

--- testing_videos/frame_generator/frame_generator.py
+++ testing_videos/frame_generator/frame_generator.py
@@ -1,12 +1,13 @@
 # © 2025 Matthew Vallance. All rights reserved.
 # COMP1682 Final Year Project.
 # Purpose: This script generates simple frames that can be used to test video processing algorithms
-# Caution: This script generates a large number of frames, don't run it unless you need to
-# Note: Remember to use the .run configuration to run this script, as it requires some exports to be set
+# Caution: This script generates a large number of frames, don't run it unless you need to. Multi-threading is heavy on resources, make sure to update the max_workers value to suit your system
+# Note: Exports for using cairo with Homebrew: PKG_CONFIG_PATH=/opt/homebrew/opt/cairo/lib/pkgconfig;LD_LIBRARY_PATH=/opt/homebrew/opt/cairo/lib;DYLD_LIBRARY_PATH=/opt/homebrew/opt/cairo/lib
 import svgwrite
 from pathlib import Path
 import shutil
 import cairosvg
+from concurrent.futures import ThreadPoolExecutor
 
 def generate_frame(object_position: tuple, frame_num: int):
     # Set up the SVG frame
@@ -33,12 +34,14 @@ for frame in range(total_frames):
     print(f'Generating frame {frame}')
     generate_frame((frame-250, 500), frame)
 
-# Then, convert all SVG frames to PNG
-for svg_frame in sorted(Path('temp_svg').iterdir(), key=lambda x: int(x.stem)):
-    # Convert SVG to PNG
+# Then, convert all SVG frames to PNG (multi-threading significantly speeds up the process)
+def convert_svg_to_png(svg_frame):
     print(f'Converting {svg_frame} to PNG')
     cairosvg.svg2png(url=str(svg_frame), write_to=str(f'red_ball_frames/{svg_frame.stem}.png'))
 
+with ThreadPoolExecutor(max_workers=16) as executor:
+    svg_frames = sorted(Path('temp_svg').iterdir(), key=lambda x: int(x.stem))
+    executor.map(convert_svg_to_png, svg_frames)
 
 # Finally, delete temporary SVG frame directory
 shutil.rmtree(Path('temp_svg'))
