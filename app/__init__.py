1d595a6869ae3fbffafb8f35b35584276f4bc4cc
Matthew Vallance <matthew.vallance@outlook.com>
2025-02-02 14:18:05 +0000

    Removed comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 # © 2024 Matthew Vallance. All rights reserved.
 # COMP1682 Final Year Project.
 # Initialization
-# pip3 install opencv-python
-# pip3 install PyQt6
 from global_tools import Tools
 
 # Create all required directories
