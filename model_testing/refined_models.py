7afd4c1c1242834de0e148484c1171e26b6e4dd1
Matthew Vallance <matthew.vallance@outlook.com>
2025-02-14 13:24:19 +0000

    Linux testing

--- model_testing/refined_models.py
+++ model_testing/refined_models.py
@@ -1,13 +1,22 @@
 # © 2025 Matthew Vallance. All rights reserved.
 # COMP1682 Final Year Project.
 # Purpose: File to demonstrate all models
+
+# ---- Imports ----
+# General imports
 import torch
 from PIL import Image
-import open_clip
 import os
 import csv
 import time
 from prettytable import PrettyTable
+
+# Import the models
+import open_clip
+import clip
+
+# Import the datasets (as needed)
+from torchvision.datasets import ImageNet
 
 # ---- General functions ----
 def print_results(model_name, run, results):
@@ -54,6 +63,11 @@ def run_test(model_name):
                 results = run_open_clip()
                 print_results(model_name, i, results)
                 all_results.append([model_name, i, results])
+        case "CLIP":
+            for i in range(3):
+                results = run_clip()
+                print_results(model_name, i, results)
+                all_results.append([model_name, i, results])
         case _:
             print("Model not found")
 
@@ -96,6 +110,53 @@ def run_open_clip():
 
     return [return_values, total_end_time - total_start_time, indv_timings]
 
+def run_clip():
+    # Create array to store all return values
+    return_values = []
+    indv_timings = []
+
+    # Time the process
+    total_start_time = time.time()
+
+    # Load the model
+    device = "cuda"
+    model, preprocess = clip.load('RN50', device)
+
+    # Download the dataset
+    dataset = ImageNet(root=os.path.expanduser("E:\\datasets\\imagenet-2012"))
+
+    # Run detection on all testing images
+    for image_path in os.listdir("testing_images"):
+        indv_start_time = time.time()
+        # Prepare the inputs
+        image = Image.open(f"testing_images\\{image_path}")
+        image_input = preprocess(image).unsqueeze(0).to(device)
+        text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in dataset.classes]).to(device)
+
+        # Calculate features
+        with torch.no_grad():
+            image_features = model.encode_image(image_input)
+            text_features = model.encode_text(text_inputs)
+
+        # Pick the top 5 most similar labels for the image
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+        values, indices = similarity[0].topk(5)
+
+        # Add features to return_values
+        return_values.append([f"{image_path} - Top predictions:"])
+        for value, index in zip(values, indices):
+            return_values[-1].append(f"{'|'.join(dataset.classes[index]):>16s}: {100 * value.item():.2f}%")
+        indv_end_time = time.time()
+        indv_timings.append(indv_end_time - indv_start_time)
+
+    # End the timer
+    total_end_time = time.time()
+
+    return [return_values, total_end_time - total_start_time, indv_timings]
+
 # ---- Run all models ----
 run_test("OpenCLIP")
+run_test("CLIP")
 print("All models have been run and the results have been saved.")
