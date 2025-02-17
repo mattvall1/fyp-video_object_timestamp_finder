d53a8683e5effb96443c81c4838f73b9d2cd204e
Matthew Vallance <matthew.vallance@outlook.com>
2025-02-17 13:15:43 +0000

    Created startup files for Azure service

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# © 2025 Matthew Vallance. All rights reserved.
+# COMP1682 Final Year Project.
+# Purpose: Run FastAPI application on Azure App Service. (Ignoring all other files, except those in the /api directory)
+# Note: Remember to configure the Azure App Service to run the startup.sh script.
+python /api/main.py
