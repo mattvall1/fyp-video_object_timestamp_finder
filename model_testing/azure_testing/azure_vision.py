09f36171ddb89fe90cf6cf70a9c1fcc41f7d1e88
Matthew Vallance <matthew.vallance@outlook.com>
2025-02-17 12:35:53 +0000

    Azure Vision API testing, compressed testing images added, results.csv kept for logging

---
+++ model_testing/azure_testing/azure_vision.py
@@ -0,0 +1,32 @@
+# © 2025 Matthew Vallance. All rights reserved.
+# COMP1682 Final Year Project.
+# Purpose: Standalone script to test azure API calls for image recognition.
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from msrest.authentication import CognitiveServicesCredentials
+import os
+
+
+# Setup endpoint and client for Azure Vision
+api_key = open('api_key.txt').read().strip()
+endpoint = "https://fyp-msvision.cognitiveservices.azure.com/"
+client = ComputerVisionClient(endpoint, credentials=CognitiveServicesCredentials(api_key))
+
+# Get all image paths
+image_paths = []
+dir_paths = os.listdir("../../testing_images/small")
+for dir_path in dir_paths:
+    if dir_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
+        image_paths.append("../../testing_images/small/"+dir_path)
+
+# Analyse an image
+for image_path in image_paths:
+    features = ["Objects"]
+    # Open image as binary stream (to send to Azure)
+    with open(image_path, "rb") as image_stream:
+        analysis = client.analyze_image_in_stream(image_stream, features)
+
+    # Print all detected objects and their confidence scores
+    print(f"\n--------------- {image_path} ---------------")
+    for obj in analysis.objects:
+        print(f"Object: {obj.object_property} (confidence: {obj.confidence:.2f})")
