9d178a616058446328ade76453ab447d15b1498e
Matthew Vallance <matthew.vallance@outlook.com>
2025-02-17 14:10:41 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 # COMP1682 Final Year Project.
 # Purpose: Run FastAPI application on Azure App Service. (Ignoring all other files, except those in the /api directory)
 # Note: Remember to configure the Azure App Service to run the startup.sh script (Settings -> Configuration -> General Settings -> Startup Command -> "startup.sh".
-python /api/main.py
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 main:app
