50a74fd59305a9edc272e880d90f315a12e9e41f
Matthew Vallance <matthew.vallance@outlook.com>
2025-02-23 16:47:07 +0000

    Rename frame searching to frame searching Azure so more models can be added

---
+++ scripts/binary_search_theory/binary_search_theory.py
@@ -0,0 +1,24 @@
+# © 2025 Matthew Vallance. All rights reserved.
+# COMP1682 Final Year Project.
+# Purpose: Script theory of binary search (effectively the first draft)
+# Note: This will only work if we know FOR DEFINITE that there is a specific object in the video - that is okay.
+# Imports
+import os
+from frame_searching_azure import FrameSearcher
+
+# Get list of all images in frames dir
+total_frames = len(os.listdir('frames'))
+
+# Get subset of frames to search (every 60th frame)
+initial_subset = []
+for frame in range(0, total_frames, 60):
+    initial_subset.append(f'{frame:04d}')
+
+# Check each image in initial subset
+for frame in initial_subset:
+    frame_search = FrameSearcher()
+
+    image = frame_search.get_image(image_path=f"frames/{frame}.jpg")
+    captions = frame_search.get_captions(image=image)
+    print(f"frames/{frame}.jpg", captions)
+
