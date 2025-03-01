10b5856d71f07f259c715ce7506b1c6923f04dfa
Matthew Vallance <matthew.vallance@outlook.com>
2025-03-01 17:43:15 +0000

    Fix multiprocessing

--- scripts/binary_search_theory/binary_search_theory.py
+++ scripts/binary_search_theory/binary_search_theory.py
@@ -4,22 +4,29 @@
 # Note: This will only work if we know FOR DEFINITE that there is a specific object in the video - that is okay.
 # Imports
 import os
+import multiprocessing
 from frame_searching_florence import FrameSearcher
 
-# Get list of all images in frames dir
-total_frames = len(os.listdir('frames'))
+def main():
+    # Get list of all images in frames dir
+    total_frames = len(os.listdir('frames'))
 
-# Get subset of frames to search (every 60th frame)
-initial_subset = []
-for frame in range(1800, total_frames, 60):
-    initial_subset.append(f'{frame:04d}')
+    # Get subset of frames to search (every 60th frame)
+    initial_subset = []
+    for frame in range(1800, total_frames, 60):
+        initial_subset.append(f'{frame:04d}')
 
-# Check each image in initial subset
-for frame in initial_subset:
+    # Create frame searcher once
     frame_search = FrameSearcher()
 
-    image = frame_search.get_image(image_path=f"frames/{frame}.jpg")
-    # Ignore first caption, as that encompasses the whole image
-    captions = frame_search.get_captions(image=image)[1:]
-    print(f"frames/{frame}.jpg", captions)
+    # Check each image in initial subset
+    for frame in initial_subset:
+        image = frame_search.get_image(image_path=f"frames/{frame}.jpg")
+        # Get caption
+        caption = frame_search.get_caption(image=image)
+        print(f"frames/{frame}.jpg", caption)
 
+if __name__ == "__main__":
+    # This fixes the multiprocessing issue
+    multiprocessing.set_start_method('spawn')
+    main()
