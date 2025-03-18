fa9c20e254ab53321035e266d1e812196d1ad639
Matthew Vallance <matthew.vallance@outlook.com>
2025-03-18 17:28:49 +0000

    Create language_handler.py

---
+++ app/processing/language_handler.py
@@ -0,0 +1,16 @@
+# © 2025 Matthew Vallance. All rights reserved.
+# COMP1682 Final Year Project.
+# Purpose: Process text inputs and outputs
+
+
+class LanguageHandler:
+    def __init__(self):
+        pass
+
+    def split_phrases(self, text):
+        # Split text into phrases
+        return text.split(".")
+
+    # Get synonyms for a word
+    def get_word_synonyms(self, text):
+        pass
