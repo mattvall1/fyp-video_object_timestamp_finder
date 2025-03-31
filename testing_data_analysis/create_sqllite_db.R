059f4f43e03914cc7adafa3d57d60bc6b72f051c
Matthew Vallance <matthew.vallance@outlook.com>
2025-03-31 21:03:42 +0100

    Better database, now including failed urls and errors

--- testing_data_analysis/create_sqllite_db.R
+++ testing_data_analysis/create_sqllite_db.R
@@ -7,20 +7,28 @@ library(RSQLite)
 library(readr)
 
 # Set paths
-csv_file_path <- "model_results.csv"
-sqlite_db_path <- "model_results.db"
-
-# Read the CSV file into a data frame
-results_csv_df <- read_csv(csv_file_path)
-
-# Convert headings to lowercase
-results_csv_df <- setNames(results_csv_df, tolower(names(results_csv_df)))
+csv_file_paths <- list("model_results.csv",
+					   "failed_urls.csv",
+					   "other_errors.csv")
+sqlite_db_path <- "testing_results.db"
 
 # Connect to the SQLite database (it will be created if it doesn't exist)
 results_db <- dbConnect(SQLite(), dbname = sqlite_db_path)
 
-# Automatically write the data frame to the SQLite database. CSV headers will be used as column names.
-dbWriteTable(results_db, "results", results_csv_df, overwrite = TRUE)
+# Loop through each CSV file path and write it to the SQLite database
+for (csv_file_path in csv_file_paths) {
+  # Read the CSV file into a data frame
+  results_csv_df <- read_csv(csv_file_path)
+
+  # Convert headings to lowercase
+  results_csv_df <- setNames(results_csv_df, tolower(names(results_csv_df)))
+
+  # Get the table name from the CSV file name
+  table_name <- gsub("\\.csv$", "", basename(csv_file_path))
+
+  # Automatically write the data frame to the SQLite database. CSV headers will be used as column names.
+  dbWriteTable(results_db, table_name, results_csv_df, overwrite = TRUE)
+}
 
 # Close the database connection
 dbDisconnect(results_db)
--- testing_data_analysis/example_queries.sql
+++ testing_data_analysis/example_queries.sql
@@ -1,8 +1,6 @@
 -- © 2025 Matthew Vallance. All rights reserved.
 -- COMP1682 Final Year Project.
--- Purpose: Simple SQL queries to check connection
+-- Purpose: Simple SQL queries to check connection (when using DataSpell IDE) and to test the database.
 
 -- Get the average score for the 'BLEU' metric for OpenCLIP model
 SELECT AVG(score) FROM results WHERE Metric == 'BLEU' AND Model == 'OpenCLIP';
-
-
