be9af527c394391e4eaeca47bff4f8b99567b08f
Matthew Vallance <matthew.vallance@outlook.com>
2025-03-31 21:33:48 +0100

    Add queries to SQL file

--- testing_data_analysis/example_queries.sql
+++ testing_data_analysis/example_queries.sql
@@ -2,5 +2,26 @@
 -- COMP1682 Final Year Project.
 -- Purpose: Simple SQL queries to check connection (when using DataSpell IDE) and to test the database.
 
--- Get the average score for the 'BLEU' metric for OpenCLIP model
-SELECT AVG(score) FROM results WHERE Metric == 'BLEU' AND Model == 'OpenCLIP';
+-- Basic statistics about the table
+SELECT COUNT(*) AS total_rows, MAX(timestamp) - MIN(timestamp) AS total_time_taken_s FROM model_results;
+
+-- Get BLEU scores for each model
+SELECT model, AVG(score) AS bleu FROM model_results WHERE metric = 'BLEU' GROUP BY model;
+
+-- Get METEOR scores for each model
+SELECT model, AVG(score) AS meteor FROM model_results WHERE metric = 'METEOR' GROUP BY model;
+
+-- Get ROUGE-1 scores for each model
+SELECT model, AVG(score) AS rouge_1 FROM model_results WHERE metric = 'ROUGE-1' GROUP BY model;
+
+-- Get ROUGE-2 scores for each model
+SELECT model, AVG(score) AS rouge_2 FROM model_results WHERE metric = 'ROUGE-2' GROUP BY model;
+
+-- Get ROUGE-L scores for each model
+SELECT model, AVG(score) AS rouge_l FROM model_results WHERE metric = 'ROUGE-L' GROUP BY model;
+
+-- Get count of failed URLs
+SELECT COUNT(url) AS failed_urls FROM failed_urls;
+
+-- Get count of other errors
+SELECT COUNT(timestamp) AS other_errors FROM other_errors;
